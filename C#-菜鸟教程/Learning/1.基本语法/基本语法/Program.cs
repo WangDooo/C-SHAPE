5c2775e3c8d77d5c9de4517d12e89ad14ca174ef
WangDooo <wangbc1993@163.com>
2018-09-15 15:19:17 +0800

    1

---
+++ C#-菜鸟教程/Learning/1.基本语法/基本语法/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace RectangleApplication {
+    class Rectangle {
+        double length;
+        double width;
+        public void Acceptdeatails() {
+            length = 4.5;
+            width = 3.5;
+        }
+        public double GetArea() {
+            return length*width;
+        }
+        public void Display() {
+            Console.WriteLine("Length:{0}",length);
+            Console.WriteLine("Width:"+width);
+            Console.WriteLine("Area:{0}",GetArea());
+        }
+    }
+    
+    class ExecuteRectangle {
+        static void Main(string[] args) {
+            Rectangle r = new Rectangle();
+            r.Acceptdeatails();
+            r.Display();
+            Console.ReadLine();
+        }
+    }
+}
