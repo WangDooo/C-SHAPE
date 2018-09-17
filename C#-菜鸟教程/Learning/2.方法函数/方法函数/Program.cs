8c4020899516a5f79e21b5c91bf3d78b17d0bb02
WangDooo <wangbc1993@163.com>
2018-09-17 19:37:14 +0800

    1

---
+++ C#-菜鸟教程/Learning/2.方法函数/方法函数/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace CalculatorApplication {
+    //可以使用类的实例从另一个类中调用其他类的公有方法
+    //class NumberManipulator {
+    //    public int FindMax(int num1, int num2) {
+    //        int result;
+    //        if (num1>num2)
+    //            result = num1;
+    //        else
+    //            result = num2;
+    //        return result;
+    //    }
+    //}
+    //class Program {
+    //    static void Main(string[] args) {
+    //        int a = 100;
+    //        int b = 200;
+    //        NumberManipulator n = new NumberManipulator();
+    //        int ans;
+    //        ans = n.FindMax(a,b);
+    //        Console.WriteLine("MAX:"+ans);
+    //        Console.ReadLine();
+    //    }
+    //}
+    class NumbermManipulator{
+        //public int factorial(int num) {
+        //    int result;
+        //    if (num == 1) {
+        //        return 1;
+        //    } else {
+        //        result = factorial(num-1)*num;
+        //        return result;
+        //    }
+        //}
+        
+        //static void Main(string[] args) {
+        //    NumbermManipulator n = new NumbermManipulator();
+        //    Console.WriteLine("6阶乘："+n.factorial(6));
+        //}
+
+
+        //引用参数是一个对变量的内存位置的引用。当按引用传递参数时，与值参数不同的是，它不会为这些参数创建一个新的存储位置。引用参数表示与提供给方法的实际参数具有相同的内存位置。
+        //在 C# 中，使用 ref 关键字声明引用参数。
+        public void swap(ref int x, ref int y) {
+            int temp;
+            temp = x;
+            x = y;
+            y = temp;
+        }
+
+        static void Main(string[] args) {
+            NumbermManipulator n = new NumbermManipulator();
+            int a = 100;
+            int b = 200;
+            n.swap(ref a, ref b);
+            Console.WriteLine("a"+a+"B"+b);
+        }
+    }
+
+
+
+}
---
+++ C#-菜鸟教程/Learning/3.数组Array/数组Array/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ArrayApplication {
+    //class Program {
+    //    static void Main(string[] args) {
+    //        double[] balance = new double[10];
+    //        balance[1] = 12.3;
+    //        Console.WriteLine(balance[1]);
+    //        double[] b2 = { 15156.3,61,25,656,99,9,9,6,6};
+    //        for (int i = 0; i < 5; i++) {
+    //            Console.WriteLine(b2[i]);
+    //        }
+    //        int [] marks = new int[]{ 44,13,123,133};
+    //        int [] socre  = marks;
+    //        // 二维数组
+    //        string [,] names;
+    //        int [ , , ] m;
+    //        int [,] a = new int [3,4]{ 
+    //            { 0,0,0,0},
+    //            { 1,2,3,4},
+    //            { 9,9,9,8}
+    //            }; // 三行4列
+    //        Console.WriteLine("a[2,3]="+a[2,3]);// a[2,3] 第三行第四列 c#也是从0开始计数的
+    //    }
+    //}
+
+    class MyArray{
+        double getAverage(int[] arr, int size) {
+            int i;
+            double avg;
+            int sum = 0;
+            for (i = 0; i < size; ++i) {
+                sum += arr[i];
+            }
+            avg = (double)sum/size;
+            return avg;
+        }
+        static void Main(string[] args) {
+            MyArray app = new MyArray();
+            int[] b1 = new int[]{ 1,2,3,4,5 };
+            double avg;
+            avg = app.getAverage(b1, b1.Length);
+            Console.WriteLine("平均值：{0}", avg);
+            Console.ReadLine();
+        }
+    }
+}
