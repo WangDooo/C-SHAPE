32e19f9df3b1b6e76404dd6eb99ef08c2558a038
WangDooo <wangbc1993@163.com>
2018-09-18 17:36:15 +0800

    1

---
+++ C#-菜鸟教程/Learning/类Class/类Class/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LineApplication {
+    class Line2 {
+        public double length;
+        public Line2() {
+            Console.WriteLine("对象已建立");
+        }
+    }
+    
+    // 构造函数的名称与类的名称完全相同，它没有任何返回类型。
+
+    class Program {
+        static void Main(string[] args) {
+            Line2 line = new Line2();
+            line.length = 1;
+            Console.WriteLine(line.length);
+        }
+    }
+}
