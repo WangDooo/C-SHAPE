e84f0549a708ee4a3e42838b112cb9b575ac7b7b
WangDooo <wangbc1993@163.com>
2018-09-18 16:32:49 +0800

    1

---
+++ C#-菜鸟教程/Learning/4.字符串String/字符串String/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace StringApplication {
+    class Program {
+        static void Main(string[] args) {
+            // 字符串的连接
+            string fname, lname;
+            fname = "Wang";
+            lname = "DOO";
+
+            string fullname = fname + lname;
+            Console.WriteLine("Fullname="+fullname);
+
+            // 使用string构造函数
+            char[] letters = { 'W','a','n','g'};
+            string namechar = new string(letters);
+            Console.WriteLine("NEWCHAR:"+namechar);
+
+            // 方法返回字符串
+            string[] sarray = { "Hello", "Wang", "Doo", "Googo"};
+            string message = String.Join("-", sarray); // 每个次之间的连接
+            Console.WriteLine(message);
+            Console.WriteLine(message.Length);
+
+            // 格式化
+            DateTime waiting = new DateTime(2018,9,18,10,41,56);
+            string chat = String.Format("{0:tt dddd yyyy MMMM HH mm ss}",waiting);
+            Console.WriteLine(chat);
+        }
+    }
+}
---
+++ C#-菜鸟教程/Learning/5.结构体Struct/结构体Struct/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+
+namespace Structure {
+    struct Books {
+        public string title;
+        public int id;
+        public void get(string t, int idd) {
+            title = t;
+            id = idd;
+        }
+        public void display() {
+            Console.WriteLine("TITLE  "+title);
+            Console.WriteLine("IDIDID  "+id);
+        }
+    };
+    
+    class Program {
+        static void Main(string[] args) {
+            Books Book1;
+            Book1.title = "qweqew";
+            Book1.id = 123;
+
+            Console.WriteLine("title："+Book1.title);
+            Console.WriteLine("id："+Book1.id);
+
+            Books Book2 = new Books();
+            Book2.get("asdsad as",999);
+            Book2.display();
+        }
+    }
+}
