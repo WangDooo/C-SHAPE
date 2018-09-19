0080efe629d105b794190b29c3f03485ff56f900
WangDooo <wangbc1993@163.com>
2018-09-19 23:00:29 +0800

    1

---
+++ C#-菜鸟教程/Learning/命名空间namespace/命名空间namespace/Program.cs
@@ -0,0 +1,63 @@
+﻿//using System;
+
+//// 在一个命名空间中声明的类的名称与另一个命名空间中声明的相同的类的名称不冲突。
+//namespace first_space {
+//    class abc {
+//        public void func() {
+//            Console.WriteLine("FIrst space");
+//        }
+//    }
+//}
+
+//namespace second_space {
+//    class abc {
+//        public void func() {
+//            Console.WriteLine("Second space");
+//        }
+//    }
+//}
+
+//namespace TestClass{
+//    class Program {
+//        static void Main(string[] args) {
+//            first_space.abc fc = new first_space.abc();
+//            second_space.abc sc = new second_space.abc();
+//            fc.func();
+//            sc.func();
+//        }
+//    }
+//}
+
+
+// using 关键字
+
+using System;
+using first_space;
+using second_space;
+
+namespace first_space {
+    class abc {
+        public void func() {
+            Console.WriteLine("FIrst space");
+        }
+    }
+}
+
+namespace second_space {
+    class abc2 {
+        public void func() {
+            Console.WriteLine("Second space2222");
+        }
+    }
+}
+
+namespace TestClass {
+    class Program {
+        static void Main(string[] args) {
+            abc fc = new abc();
+            abc2 sc = new abc2();
+            fc.func();
+            sc.func();
+        }
+    }
+}
---
+++ C#-菜鸟教程/Learning/异常处理/异常处理/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ErrorHandlingApplication {
+    class DivNumbers {
+        int result;
+        DivNumbers() {
+            result = 0;
+        }
+        public void division(int num1, int num2) {
+            try {
+                result = num1 / num2;
+            }
+            catch(DivideByZeroException e) {
+                Console.WriteLine("Exception caught: {0}",e);
+            } finally {
+                Console.WriteLine("Result"+result);
+            }
+        }
+
+        static void Main(string[] args) {
+            DivNumbers d = new DivNumbers();
+            d.division(12,0);
+        }     
+    }
+}
