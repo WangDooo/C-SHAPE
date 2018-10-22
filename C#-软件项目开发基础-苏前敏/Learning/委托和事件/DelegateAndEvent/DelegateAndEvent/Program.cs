32c4bd0907b05dbdee8b556611b024d4767efd74
WangDooo <wangbc1993@163.com>
2018-10-22 21:40:26 +0800

    1

---
+++ C#-软件项目开发基础-苏前敏/Learning/委托和事件/DelegateAndEvent/DelegateAndEvent/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate {
+    class Program {
+        public delegate void DelegateFun(int x, int y);
+        static void Main(string[] args) {
+            DelegateFun fun;
+            int x = 4;
+            int y = 2;
+            fun = new DelegateFun(fun1);
+            fun(x,y);
+            fun = fun2;
+            fun(x,y);
+            fun = fun3;
+            fun(x,y);
+
+            fun = fun1;
+            fun += fun2;
+            fun += fun3;
+            fun -= fun1;
+            fun(x,y);
+            Console.ReadLine();
+        }
+        public static void fun1(int x, int y) {
+            Console.WriteLine("x+y={0}", x+y);
+        }
+        public static void fun2(int x, int y) {
+            Console.WriteLine("x-y={0}", x-y);
+        }
+        public static void fun3(int x, int y) {
+            Console.WriteLine("x*y={0}", x*y);
+        }
+    }
+}
---
+++ C#-软件项目开发基础-苏前敏/Learning/委托和事件/DelegateAndEvent/Event/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Event {
+    class EventUtilClass {
+        public delegate void EventUtilEventHandler(object sender, System.EventArgs e); // 定义delegate对象
+        public class EventClass {
+            public void EventUtil_Function(object sender, System.EventArgs e) {
+                Console.WriteLine("using event to print!");
+            }
+        }
+        static void Main(string[] args) {
+        }
+    }
+}
