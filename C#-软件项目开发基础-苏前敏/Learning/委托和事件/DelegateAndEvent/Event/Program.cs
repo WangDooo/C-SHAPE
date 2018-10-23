973e674bf197e0eb13c854975b491d9225f29765
WangDooo <wangbc1993@163.com>
2018-10-23 14:42:04 +0800

    1

--- C#-软件项目开发基础-苏前敏/Learning/委托和事件/DelegateAndEvent/Event/Program.cs
+++ C#-软件项目开发基础-苏前敏/Learning/委托和事件/DelegateAndEvent/Event/Program.cs
@@ -6,11 +6,21 @@ using System.Threading.Tasks;
 
 namespace Event {
     class EventUtilClass {
-        public delegate void EventUtilEventHandler(object sender, System.EventArgs e); // 定义delegate对象
+        public delegate void EventUtilEventHandler(object sender, System.EventArgs e); // 1. 定义delegate对象
         public class EventClass {
-            public void EventUtil_Function(object sender, System.EventArgs e) {
+            public void EventUtil_Function(object sender, System.EventArgs e) { // 3. 定义处理事件的方法 与delegate对象有相同的参数
                 Console.WriteLine("using event to print!");
             }
+        }
+
+        // 4. 用event关键词 定义事件对象
+        private event EventUtilEventHandler utilEvent;
+        private EventClass eventClass;
+
+        // 构造函数
+        public EventUtilClass() {
+            eventClass = new EventClass();
+            this.utilEvent += new EventUtilEventHandler(eventClass.EventUtil_Function);
         }
         static void Main(string[] args) {
         }
