fa0b93aa127ed9a2b8bbbad260e734abca2b52d3
WangDooo <wangbc1993@163.com>
2018-10-24 15:13:50 +0800

    1

---
+++ C#-软件项目开发基础-苏前敏/Learning/图片处理/BitmapForm/BitmapForm/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace BitmapForm {
+    partial class Form1 {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(41, 26);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(190, 260);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(256, 26);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(190, 260);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(469, 26);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(190, 260);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(256, 306);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(190, 42);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "底片效果";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(469, 306);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(190, 42);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "浮雕效果";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(721, 385);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ C#-软件项目开发基础-苏前敏/Learning/图片处理/BitmapForm/BitmapForm/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BitmapForm {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            int Height = this.pictureBox1.Image.Height;
+            int Width = this.pictureBox1.Image.Width;
+            Bitmap newbitmap = new Bitmap(Width, Height);
+            Bitmap oldbitmap = (Bitmap)this.pictureBox1.Image;
+            Color pixel;
+            for (int x = 1; x < Width; x++) {
+                for(int y = 1; y < Height; y++) {
+                    int r,g,b;
+                    pixel = oldbitmap.GetPixel(x,y);
+                    r = 255 - pixel.R;
+                    g = 255 - pixel.G;
+                    b = 255 - pixel.B;
+                    newbitmap.SetPixel(x,y,Color.FromArgb(r,g,b));
+                }
+            }
+            this.pictureBox2.Image = newbitmap;
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            int Height = this.pictureBox1.Image.Height;
+            int Width = this.pictureBox1.Image.Width;
+            Bitmap newbitmap = new Bitmap(Width, Height);
+            Bitmap oldbitmap = (Bitmap)this.pictureBox1.Image;
+            Color pixel1, pixel2;
+            for (int x = 1; x < Width-1; x++) {
+                for(int y = 1; y < Height-1; y++) {
+                    int r =0 ,g = 0 ,b = 0;
+                    pixel1 = oldbitmap.GetPixel(x,y);
+                    pixel2 = oldbitmap.GetPixel(x+1,y+1);
+                    r = Math.Abs(pixel1.R - pixel2.R + 128);
+                    g = Math.Abs(pixel1.G - pixel2.G + 128);
+                    b = Math.Abs(pixel1.B - pixel2.B + 128);
+                    if (r > 255)
+                        r = 255;
+                    if (g > 255)
+                        g = 255;
+                    if (b > 255)
+                        b = 255;
+                    newbitmap.SetPixel(x,y,Color.FromArgb(r,g,b));
+                }
+            }
+            this.pictureBox3.Image = newbitmap;
+        }
+    }
+}
