1fd0e469b77da3e545f73cd8894d84ea8f715ec4
WangDooo <wangbc1993@163.com>
2018-11-05 14:33:48 +0800

    1

---
+++ C#-菜鸟教程/Learning/数组、ArrayList、List区别/ListAndArray/ListAndArray/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListAndArray {
+    class Program {
+        static void Main(string[] args) {
+            List<double> list = new List<double>();
+            for (int i = 0; i < 4; i++) {
+                double temp =i*0.63;
+                list.Add(temp);
+            }
+            double[] arr = {1.2,2.3,5.6};
+            list.AddRange(arr);
+            list.Insert(3,999);
+            list.Insert(4,999);
+            list.Sort();
+
+            foreach(double t in list) {
+                Console.WriteLine(t);
+            }
+            for(int i = 0; i < list.Count(); i++) {
+                Console.Write(list[i]);
+            }
+            Console.WriteLine(list[2]);
+        }
+    }
+}
