2f601a2dea8be56db71d88217209b4b36f307153
WangDooo <wangbc1993@163.com>
2019-02-12 22:18:52 +0800

    1

--- C#-杂学/文件加密解密/EncryptionForms/EncryptionForms/Form1.Designer.cs
+++ C#-杂学/文件加密解密/EncryptionForms/EncryptionForms/Form1.Designer.cs
@@ -23,20 +23,133 @@
         /// 使用代码编辑器修改此方法的内容。
         /// </summary>
         private void InitializeComponent() {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(31, 46);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(179, 67);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "DES加密保存";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(31, 119);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(179, 68);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "解密读取";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(269, 46);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(569, 141);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(269, 226);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(569, 174);
+            this.textBox2.TabIndex = 3;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(31, 226);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(179, 65);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "DES保存";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(31, 297);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(179, 59);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "DES读取";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(269, 430);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(569, 126);
+            this.textBox3.TabIndex = 6;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(31, 413);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(179, 58);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "转换保存";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(31, 489);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(179, 67);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "转换读取";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(916, 447);
+            this.ClientSize = new System.Drawing.Size(916, 578);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "EncryptionForm";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
     }
 }
 
--- C#-杂学/文件加密解密/EncryptionForms/EncryptionForms/Form1.cs
+++ C#-杂学/文件加密解密/EncryptionForms/EncryptionForms/Form1.cs
@@ -7,11 +7,313 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Security.Cryptography;
+using System.Globalization;
 
 namespace EncryptionForms {
     public partial class Form1 : Form {
         public Form1() {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            // 利用SaveFileDialog，让用户指定文件的路径名
+            SaveFileDialog saveDlg = new SaveFileDialog();
+            saveDlg.Filter = "文本文件|*.txt";
+            if (saveDlg.ShowDialog() == DialogResult.OK)
+            { 
+
+                FileEncryptDecrypt.Encrypt(saveDlg.FileName,textBox1.Text);
+
+                // 提示用户：文件保存的位置和文件名
+                MessageBox.Show("文件已成功保存到" + saveDlg.FileName);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            string filepath = @"C:\DDD\GitHub\C-SHAPE\C#-杂学\文件加密解密\test2.txt";
+            MessageBox.Show(FileEncryptDecrypt.Decrypt(filepath));
+        }
+
+        private void button3_Click(object sender, EventArgs e) {
+            SaveFileDialog saveDlg = new SaveFileDialog();
+            saveDlg.Filter = "文本文件|*.txt";
+            if (saveDlg.ShowDialog() == DialogResult.OK)
+            { 
+                FileStream fs = new FileStream(saveDlg.FileName, FileMode.Append, FileAccess.Write);
+                // string encrypt = Encrypt(textBox2.Text);
+                string encrypt = EncryptString(textBox2.Text);
+                byte[] data = System.Text.Encoding.Default.GetBytes(encrypt);
+                //开始写入
+                fs.Write(data, 0, data.Length);
+                //清空缓冲区、关闭流
+                fs.Flush();
+                fs.Close();
+                // 提示用户：文件保存的位置和文件名
+                MessageBox.Show("文件已成功保存到" + saveDlg.FileName);
+            }
+
+        }
+
+        private void button4_Click(object sender, EventArgs e) {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Multiselect = false; //该值确定是否可以选择多个文件
+            dialog.Title = "请选择文件夹";
+            dialog.Filter = "文本文件|*.txt";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                string file = dialog.FileName;
+                FileStream fs = new FileStream(file, FileMode.Open);
+                StreamReader sr = new StreamReader(fs);
+                string line = sr.ReadToEnd();
+                // string decrypt = Decrypt(line);
+                string decrypt = DecryptString(line);
+                textBox3.Text = decrypt;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e) {
+            SaveFileDialog saveDlg = new SaveFileDialog();
+            saveDlg.Filter = "文本文件|*.txt";
+            if (saveDlg.ShowDialog() == DialogResult.OK)
+            { 
+                FileStream fs = new FileStream(saveDlg.FileName, FileMode.Append, FileAccess.Write);
+                byte[] result = Encoding.UTF8.GetBytes(textBox2.Text);
+                string encrypt = BitConverter.ToString(result);
+                byte[] data = System.Text.Encoding.Default.GetBytes(encrypt);
+                //开始写入
+                fs.Write(data, 0, data.Length);
+                //清空缓冲区、关闭流
+                fs.Flush();
+                fs.Close();
+                // 提示用户：文件保存的位置和文件名
+                MessageBox.Show("文件已成功保存到" + saveDlg.FileName);
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e) {
+
+        }
+
+        public static string Key = "WangDooo";
+
+        public static string EncryptString(string sInputString)
+        {
+            try
+            {
+                byte[] data = Encoding.UTF8.GetBytes(sInputString);
+ 
+                DESCryptoServiceProvider DES = new DESCryptoServiceProvider();
+ 
+                DES.Key = ASCIIEncoding.ASCII.GetBytes(Key);
+ 
+                DES.IV = ASCIIEncoding.ASCII.GetBytes(Key);
+ 
+                ICryptoTransform desencrypt = DES.CreateEncryptor();
+ 
+                byte[] result = desencrypt.TransformFinalBlock(data, 0, data.Length);
+ 
+                return BitConverter.ToString(result);
+            }
+            catch { }
+ 
+            return "转换出错！";
+        }
+
+        public static string DecryptString(string sInputString)
+        {
+            try
+            {
+                string[] sInput = sInputString.Split("-".ToCharArray());
+ 
+                byte[] data = new byte[sInput.Length];
+ 
+                for (int i = 0; i < sInput.Length; i++)
+                {
+                    data[i] = byte.Parse(sInput[i], NumberStyles.HexNumber);
+                }
+ 
+                DESCryptoServiceProvider DES = new DESCryptoServiceProvider();
+ 
+                DES.Key = ASCIIEncoding.ASCII.GetBytes(Key);
+ 
+                DES.IV = ASCIIEncoding.ASCII.GetBytes(Key);
+ 
+                ICryptoTransform desencrypt = DES.CreateDecryptor();
+ 
+                byte[] result = desencrypt.TransformFinalBlock(data, 0, data.Length);
+ 
+                return Encoding.UTF8.GetString(result);
+            }
+            catch { }
+ 
+            return "解密出错！";
+        }
+
+        public static string Encrypt(string sourceString)
+        {
+            try
+            {
+                byte[] btKey = Encoding.UTF8.GetBytes(Key);
+ 
+                byte[] btIV = Encoding.UTF8.GetBytes(Key);
+ 
+                DESCryptoServiceProvider des = new DESCryptoServiceProvider();
+ 
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    byte[] inData = Encoding.UTF8.GetBytes(sourceString);
+                    try
+                    {
+                        using (CryptoStream cs = new CryptoStream(ms, des.CreateEncryptor(btKey, btIV), CryptoStreamMode.Write))
+                        {
+                            cs.Write(inData, 0, inData.Length);
+ 
+                            cs.FlushFinalBlock();
+                        }
+ 
+                        return Convert.ToBase64String(ms.ToArray());
+                    }
+                    catch
+                    {
+                        return sourceString;
+                    }
+                }
+            }
+            catch { }
+ 
+            return "DES加密出错";
+        }
+
+        public static string Decrypt(string encryptedString)
+        {
+            byte[] btKey = Encoding.UTF8.GetBytes(Key);
+ 
+            byte[] btIV = Encoding.UTF8.GetBytes(Key);
+ 
+            DESCryptoServiceProvider des = new DESCryptoServiceProvider();
+ 
+            using (MemoryStream ms = new MemoryStream())
+            {
+                byte[] inData = Convert.FromBase64String(encryptedString);
+                try
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, des.CreateDecryptor(btKey, btIV), CryptoStreamMode.Write))
+                    {
+                        cs.Write(inData, 0, inData.Length);
+ 
+                        cs.FlushFinalBlock();
+                    }
+ 
+                    return Encoding.UTF8.GetString(ms.ToArray());
+                }
+                catch
+                {
+                    return encryptedString;
+                }
+            }
+        }
+
+        
+    }
+
+
+
+    /// <summary>
+    /// 文件加密
+    /// </summary>
+    public class FileEncryptDecrypt
+    {
+
+        /// <summary>
+        /// 加密字符
+        /// </summary>
+         private static string key = "KANGJINW";
+
+        /// <summary>
+        /// 将密钥加密写入到文件
+        /// </summary>
+        /// <param name="sInputFilename">密密钥</param>
+        /// <param name="sOutputFilename">密钥文件路径</param>
+        /// <param name="sKey"></param>
+        private  static void EncryptFile(string sInputFilename,string sOutputFilename,string sKey)
+        {
+            FileStream fsEncrypted = new FileStream(sOutputFilename, FileMode.Append, FileAccess.Write);
+            DESCryptoServiceProvider DES = new DESCryptoServiceProvider();
+            DES.Mode = CipherMode.ECB; //这里指定加密模式为ECB
+            DES.Key = ASCIIEncoding.ASCII.GetBytes(sKey);
+            DES.IV = ASCIIEncoding.ASCII.GetBytes(sKey);
+            ICryptoTransform desencrypt = DES.CreateEncryptor();
+            CryptoStream cryptostream = new CryptoStream(fsEncrypted,  desencrypt, CryptoStreamMode.Write);
+            byte[] fsInput = System.Text.Encoding.Default.GetBytes(sInputFilename);
+            cryptostream.Write(fsInput, 0, fsInput.Length);
+            cryptostream.Close();
+            fsEncrypted.Close();
+        }
+        /// <summary>
+        /// 打开密钥文件
+        /// </summary>
+        /// <param name="sInputFilename">密钥路径</param>
+        /// <param name="sKey"></param>
+        static string DecryptFile(string sInputFilename, string sKey)
+        {
+            DESCryptoServiceProvider DES = new DESCryptoServiceProvider();
+            DES.Mode = CipherMode.ECB; //这里指定加密模式为ECB
+            DES.Key = ASCIIEncoding.ASCII.GetBytes(sKey);
+
+            DES.IV = ASCIIEncoding.ASCII.GetBytes(sKey);
+            DES.Padding = System.Security.Cryptography.PaddingMode.Zeros; // None
+
+            FileStream fsread = new FileStream(sInputFilename,  FileMode.Open,  FileAccess.Read);
+
+            ICryptoTransform desdecrypt = DES.CreateDecryptor();
+
+            CryptoStream cryptostreamDecr = new CryptoStream(fsread,  desdecrypt,  CryptoStreamMode.Read);
+            StreamReader read = new StreamReader(cryptostreamDecr, System.Text.Encoding.Default);
+            string reft = read.ReadToEnd();
+            fsread.Flush();
+            fsread.Close();
+            return reft;
+        }
+        /// <summary>
+        /// 加密文件
+        /// </summary>
+        /// <param name="filename">文件存放路径</param>
+        /// <param name="soce">加密内容</param>
+        public static void Encrypt(string filename, string soce)
+        {
+            EncryptFile(soce, filename, key);
+            //try
+            //{
+               
+            //}
+            //catch (Exception ex)
+            //{
+
+            //    throw ex;
+            //}
+
+        }
+        /// <summary>
+        /// 解密文件
+        /// </summary>
+        /// <param name="filename">打开文件路径</param>
+        /// <returns>返回加密文件的内容</returns>
+        public static string Decrypt(string filename)
+        {
+            return DecryptFile(filename, key);
+            //try
+            //{
+
+            //    return DecryptFile(filename, key);
+
+            //}
+            //catch (Exception ex)
+            //{
+
+            //    throw ex;
+            //}
         }
     }
 }
