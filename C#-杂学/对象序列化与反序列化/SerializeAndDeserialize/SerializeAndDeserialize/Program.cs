ec208cf602cc6ee85acf879a61a7d364b06e6954
WangDooo <wangbc1993@163.com>
2019-02-13 22:23:16 +0800

    1

---
+++ C#-杂学/对象序列化与反序列化/SerializeAndDeserialize/SerializeAndDeserialize/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerializeAndDeserialize {
+    class Program {
+        static void Main(string[] args) {
+            string p = "123";
+            Console.WriteLine(p);
+            Console.Read();
+        }
+    }
+}
