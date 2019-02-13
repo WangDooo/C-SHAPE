720f23ffe375f6f811954b9a18ef694b3bf57b45
WangDooo <wangbc1993@163.com>
2019-02-13 22:53:15 +0800

    1

--- C#-杂学/对象序列化与反序列化/SerializeAndDeserialize/SerializeAndDeserialize/Program.cs
+++ C#-杂学/对象序列化与反序列化/SerializeAndDeserialize/SerializeAndDeserialize/Program.cs
@@ -3,13 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
 
 namespace SerializeAndDeserialize {
     class Program {
         static void Main(string[] args) {
             string p = "123";
+            SerializeNow();
+            DeSerializeNow();
             Console.WriteLine(p);
             Console.Read();
         }
+
+        public static void SerializeNow() {
+            ClassToSerialize c = new ClassToSerialize();
+            FileStream fileStream = new FileStream("C:\\DDD\\temp\\temp.zph", FileMode.Create);
+            BinaryFormatter b = new BinaryFormatter();
+            b.Serialize(fileStream, c);
+            fileStream.Close();
+        }
+
+        public static void DeSerializeNow() {
+            ClassToSerialize c = new ClassToSerialize();
+            c.Sex = "FFFF";
+            FileStream fileStream = new FileStream("C:\\DDD\\temp\\temp.zph", FileMode.Open, FileAccess.Read, FileShare.Read);
+            BinaryFormatter b = new BinaryFormatter();
+            c = b.Deserialize(fileStream) as ClassToSerialize;
+            Console.Write(c.name);
+            Console.Write(c.Sex);
+            fileStream.Close();
+        }
     }
+
+    [Serializable]
+    public class ClassToSerialize {
+        public int id = 100;
+        public string name = "Name";
+        [NonSerialized]
+        public string Sex = "Male";
+    }
+
+    
 }
